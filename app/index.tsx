74609407c15b9a602a2edf954d483a9d0c524d25
Byron Karlen <byronkarlen@gmail.com>
2026-04-14 23:14:38 -0700

    cleanup convex starter

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,6 @@
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
-  const tasks = useQuery(api.tasks.get);
   return (
     <View
       style={{
@@ -12,7 +9,6 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      {tasks?.map(({ _id, text }) => <Text key={_id}>{text}</Text>)}
     </View>
   );
 }
